fe24fd33e732ee062186eefe16431467c5f076d7
nbari <nbari@tequila.io>
2016-11-17 14:28:12 +0100

    modified:   ssl.go

--- cmd/www/main.go
+++ cmd/www/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
 
 	"github.com/nbari/violetear"
 	"github.com/nbari/www"
@@ -16,9 +17,9 @@ var version string
 func main() {
 	var (
 		p   = flag.Int("p", 8000, "Listen on `port`")
-		r   = flag.String("r", ".", "document `root` path")
+		r   = flag.String("r", ".", "Document `root` path")
 		s   = flag.Bool("s", false, "`silent` or quiet mode")
-		ssl = flag.Bool("ssl", false, "use `SSL` https://")
+		ssl = flag.Bool("ssl", false, "Enable `SSL` https://")
 		v   = flag.Bool("v", false, fmt.Sprintf("Print version: %s", version))
 	)
 
@@ -27,12 +28,6 @@ func main() {
 	if *v {
 		fmt.Printf("%s\n", version)
 		os.Exit(0)
-	}
-
-	err := www.CreateSSL()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
 	}
 
 	// start web server
@@ -45,12 +40,17 @@ func main() {
 		),
 	)
 
-	// maybe a string so it could define the path of where to store the cert
+	// enable ssl
 	if *ssl {
+		err := www.CreateSSL("/tmp")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 		log.Fatal(http.ListenAndServeTLS(
 			fmt.Sprintf(":%d", *p),
-			".www.pem",
-			".www.key",
+			path.Join("/tmp", ".www.pem"),
+			path.Join("/tmp", ".www.key"),
 			router),
 		)
 	} else {
